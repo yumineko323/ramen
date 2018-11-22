155d3c3c5bd737bb1792711bd001a79282c45146
tamurayy <sweet.clover0503@gmail.com>
2018-11-22 11:36:30 +0900

    上移動

--- SCR2-9/Tamura.java
+++ SCR2-9/Tamura.java
@@ -14,6 +14,8 @@ public class Tamura extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if (Greenfoot.isKeyDown("up")){
+                setRotation(270);
+                move(4);}
     }    
 }
